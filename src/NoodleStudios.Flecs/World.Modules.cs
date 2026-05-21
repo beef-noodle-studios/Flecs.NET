0f238b5c8a50ee12df17cdb9796e0bfe37caa23d
Joseph Crump <joseph.crump119@gmail.com>
2026-05-21 14:26:51 -0400

    Create World.Modules.cs

---
+++ src/NoodleStudios.Flecs/World.Modules.cs
@@ -0,0 +1,11 @@
+﻿using static Flecs.NET.Bindings.flecs;
+
+namespace NoodleStudios.Flecs;
+
+public unsafe partial struct World
+{
+    // TODO: Add World module API methods here.
+    // https://www.flecs.dev/flecs/group__c__addons__module.html
+
+    // Create, Import, etc.
+}
