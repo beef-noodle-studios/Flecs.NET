2b60fb834d2026db66e5f8a43e476724affa7788
Joseph Crump <joseph.crump119@gmail.com>
2026-05-21 14:20:17 -0400

    Add partials for Components and Pairs

---
+++ src/NoodleStudios.Flecs/World.Pairs.cs
@@ -0,0 +1,11 @@
+﻿using static Flecs.NET.Bindings.flecs;
+
+namespace NoodleStudios.Flecs;
+
+public unsafe partial struct World
+{
+    // TODO: Add World pairs API methods here.
+    // https://www.flecs.dev/flecs/group__adding__removing.html
+
+    // Add, remove, set, get, get-by-ref, tryget, has, etc.
+}
