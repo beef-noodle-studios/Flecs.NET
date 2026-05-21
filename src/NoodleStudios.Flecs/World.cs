12a6cabf5f99f2d1d7ab486140628e1c68d1752e
Joseph Crump <joseph.crump119@gmail.com>
2026-05-21 13:25:21 -0400

    Update World.cs

--- src/NoodleStudios.Flecs/World.cs
+++ src/NoodleStudios.Flecs/World.cs
@@ -10,6 +10,14 @@ public unsafe readonly struct World : IDisposable
 {
     private readonly ecs_world_t* _handle;
 
+    /// <summary>
+    ///     Create a new Flecs world.
+    /// </summary>
+    public World()
+    {
+        _handle = ecs_init();
+    }
+
     private World(ecs_world_t* handle)
     {
         _handle = handle;
@@ -19,20 +27,6 @@ public unsafe readonly struct World : IDisposable
     ///     The native Flecs world handle.
     /// </summary>
     internal ecs_world_t* Handle => _handle;
-
-    /// <summary>
-    ///     Create a new Flecs world.
-    /// </summary>
-    /// <returns>
-    ///     A <see cref="World"/> instance that wraps a newly created native
-    ///     Flecs world.
-    /// </returns>
-    public static World New()
-    {
-        ecs_world_t* handle = ecs_init();
-        var world = FromNativeWorldHandle(handle);
-        return world;
-    }
 
     /// <summary>
     ///     Create a <see cref="World"/> from <paramref name="handle"/>.
@@ -80,6 +74,62 @@ public unsafe readonly struct World : IDisposable
     }
 
     /// <summary>
+    ///     Signal exit.
+    /// </summary>
+    /// <remarks>
+    ///     This operation signals that the application should quit.
+    ///     It will cause <see cref="Update(float)"/> to return false.
+    /// </remarks>
+    public void Quit()
+    {
+        ecs_quit(_handle);
+    }
+
+    /// <summary>
+    ///     Return whether a quit has been requested.
+    /// </summary>
+    /// <returns>
+    ///     Whether a quit has been requested.
+    /// </returns>
+    public bool ShouldQuit()
+    {
+        return ecs_should_quit(_handle);
+    }
+
+    /// <summary>
+    ///     Progress a world.
+    /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         This operation progresses the world by running all systems that
+    ///         are both enabled and periodic on their matching entities.
+    ///     </para>
+    ///     <para>
+    ///         An application can pass a delta_time into the function, which
+    ///         is the time passed since the last frame. This value is passed
+    ///         to systems so they can update entity values proportional to the
+    ///         elapsed time since their last invocation.
+    ///     </para>
+    ///     <para>
+    ///         When an application passes 0 to delta_time, ecs_progress() will
+    ///         automatically measure the time passed since the last frame.
+    ///         If an application does not use time management, it should pass
+    ///         a non-zero value for delta_time (1.0 is recommended).
+    ///         That way, no time will be wasted measuring the time.
+    ///     </para>
+    /// </remarks>
+    /// <param name="deltaTime">
+    ///     The time passed since the last frame.
+    /// </param>
+    /// <returns>
+    ///     False if <see cref="Quit"/> has been called, true otherwise.
+    /// </returns>
+    public bool Update(float deltaTime)
+    {
+        return ecs_progress(_handle, deltaTime);
+    }
+
+    /// <summary>
     ///     Create new entity ID.
     /// </summary>
     /// <remarks>
@@ -91,7 +141,7 @@ public unsafe readonly struct World : IDisposable
     /// <returns>
     ///     The new entity ID.
     /// </returns>
-    public Entity CreateEmptyEntity()
+    public Entity CreateEntity()
     {
          EntityId entityId = ecs_new(_handle);
          return new Entity(entityId);
