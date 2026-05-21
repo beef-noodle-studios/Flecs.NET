83d542e87243357e846d935d153a3d3806e496ba
Joseph Crump <joseph.crump119@gmail.com>
2026-05-21 13:17:14 -0400

    Create Entity and EntityId

---
+++ src/NoodleStudios.Flecs/Entity.cs
@@ -0,0 +1,38 @@
+﻿namespace NoodleStudios.Flecs;
+
+/// <summary>
+///     Value type that represents an entity in a Flecs world.
+///     This is a thin wrapper around an <see cref="EntityId"/> with support
+///     for implicit conversion.
+/// </summary>
+/// <param name="id">
+///     The <see cref="EntityId"/> that uniquely identifies this entity.
+/// </param>
+public readonly struct Entity(EntityId id) : IEquatable<Entity>
+{
+    /// <summary>
+    ///     The <see cref="EntityId"/> that uniquely identified this entity.
+    /// </summary>
+    public readonly EntityId Id = id;
+
+    /// <summary>
+    ///     Value representing an invalid entity.
+    /// </summary>
+    public static Entity None => new(EntityId.Invalid);
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Entity other)
+            return false;
+
+        return Equals(other);
+    }
+
+    public bool Equals(Entity other) => Id.Equals(other.Id);
+    public override int GetHashCode() => Id.GetHashCode();
+
+    public static implicit operator EntityId(Entity entity) => entity.Id;
+
+    public static bool operator ==(Entity left, Entity right) => left.Equals(right);
+    public static bool operator !=(Entity left, Entity right) => !left.Equals(right);
+}
--- src/NoodleStudios.Flecs/World.cs
+++ src/NoodleStudios.Flecs/World.cs
@@ -78,4 +78,39 @@ public unsafe readonly struct World : IDisposable
             Console.WriteLine("Failed to finalize Flecs world. Error code: " + result);
         }
     }
+
+    /// <summary>
+    ///     Create new entity ID.
+    /// </summary>
+    /// <remarks>
+    ///     This operation returns an unused entity ID.
+    ///     This operation is guaranteed to return an empty entity as it does
+    ///     not use values set by <see cref="ecs_set_scope(ecs_world_t*, ulong)"/>
+    ///     or <see cref="ecs_set_with(ecs_world_t*, ulong)"/>.
+    /// </remarks>
+    /// <returns>
+    ///     The new entity ID.
+    /// </returns>
+    public Entity CreateEmptyEntity()
+    {
+         EntityId entityId = ecs_new(_handle);
+         return new Entity(entityId);
+    }
+
+    /// <summary>
+    ///     Delete an entity.
+    /// </summary>
+    /// <remarks>
+    ///     This operation will delete an entity and all of its components.
+    ///     The entity ID will be made available for recycling.
+    ///     If the entity passed to <see cref="DestroyEntity(Entity)"/> is not
+    ///     alive, the operation will have no side effects.
+    /// </remarks>
+    /// <param name="entity">
+    ///     The <see cref="Entity"/> to destroy.
+    /// </param>
+    public void DestroyEntity(Entity entity)
+    {
+        ecs_delete(_handle, entity.Id);
+    }
 }
