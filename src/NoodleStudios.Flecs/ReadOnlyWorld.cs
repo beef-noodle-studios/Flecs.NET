60ed50e1e66b6569b45b4ea9897ed3fbfbd0d289
Joseph Crump <joseph.crump119@gmail.com>
2026-05-21 11:59:22 -0400

    Configure conversion to mutable and readonly worlds

--- src/NoodleStudios.Flecs/ReadOnlyWorld.cs
+++ src/NoodleStudios.Flecs/ReadOnlyWorld.cs
@@ -1,11 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
+using static Flecs.NET.Bindings.flecs;
 
 namespace NoodleStudios.Flecs;
 
-internal class ReadOnlyWorld
+/// <summary>
+///     A read-only wrapper around a native Flecs world.
+/// </summary>
+public unsafe readonly struct ReadOnlyWorld
 {
+    private readonly ecs_world_t* _handle;
+
+    private ReadOnlyWorld(ecs_world_t* handle)
+    {
+        _handle = handle;
+    }
+
+    /// <summary>
+    ///     The native Flecs world handle.
+    /// </summary>
+    internal ecs_world_t* Handle => _handle;
+
+    /// <summary>
+    ///     Create a <see cref="ReadOnlyWorld"/> from <paramref name="handle"/>.
+    /// </summary>
+    /// <param name="handle">
+    ///     A pointer to a native Flecs world.
+    /// </param>
+    /// <returns>
+    ///     A <see cref="ReadOnlyWorld"/> instance that wraps the provided
+    ///     native world handle.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     Thrown if <paramref name="handle"/> is null.
+    /// </exception>
+    public static ReadOnlyWorld FromNativeWorldHandle(ecs_world_t* handle)
+    {
+        if (handle == null)
+            throw new ArgumentNullException(nameof(handle));
+
+        var world = new ReadOnlyWorld(handle);
+        return world;
+    }
+
+    /// <summary>
+    ///     Implicitly convert from a read-write <see cref="World"/> wrapper to
+    ///     a <see cref="ReadOnlyWorld"/> wrapper.
+    /// </summary>
+    /// <param name="world">
+    ///     The read-write world wrapper to convert.
+    /// </param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator ReadOnlyWorld(World world) => FromNativeWorldHandle(world.Handle);
 }
--- src/NoodleStudios.Flecs/World.cs
+++ src/NoodleStudios.Flecs/World.cs
@@ -1,6 +1,53 @@
-﻿namespace NoodleStudios.Flecs;
+﻿using System.Runtime.CompilerServices;
+using static Flecs.NET.Bindings.flecs;
 
-public unsafe class World
+namespace NoodleStudios.Flecs;
+
+/// <summary>
+///     A read-write wrapper around a native Flecs world.
+/// </summary>
+public unsafe readonly struct World
 {
+    private readonly ecs_world_t* _handle;
 
+    private World(ecs_world_t* handle)
+    {
+        _handle = handle;
+    }
+
+    /// <summary>
+    ///     The native Flecs world handle.
+    /// </summary>
+    internal ecs_world_t* Handle => _handle;
+
+    /// <summary>
+    ///     Create a <see cref="World"/> from <paramref name="handle"/>.
+    /// </summary>
+    /// <param name="handle">
+    ///     A pointer to a native Flecs world.
+    /// </param>
+    /// <returns>
+    ///     A <see cref="ReadOnlyWorld"/> instance that wraps the provided
+    ///     native world handle.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     Thrown if <paramref name="handle"/> is null.
+    /// </exception>
+    public static World FromNativeWorldHandle(ecs_world_t* handle)
+    {
+        if (handle == null)
+            throw new ArgumentNullException(nameof(handle));
+
+        var world = new World(handle);
+        return world;
+    }
+
+    /// <summary>
+    ///     Convert this world into a read-only wrapper.
+    /// </summary>
+    /// <returns>
+    ///     A <see cref="ReadOnlyWorld"/> instance that wraps this world.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ReadOnlyWorld AsReadOnly() => ReadOnlyWorld.FromNativeWorldHandle(_handle);
 }
