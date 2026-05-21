986f8ddef00795ac9d80e36477206895292ca288
Joseph Crump <joseph.crump119@gmail.com>
2026-05-21 14:38:55 -0400

    Partial world (#4)
    * Change World and ReadOnlyWorld to partial
    
    * Create partial World stubs
    
    * Move Frame methods to World.Frames
    
    * Move Entity API methods to World.Entities
    
    * Useful usings and unsafe modifiers
    
    * Remove unhelpful world divisions
    
    * Lock in World.Frames
    
    * Add partials for Components and Pairs
    
    * Add World.Systems
    
    * Create World.Modules.cs

---
+++ src/NoodleStudios.Flecs/World.Systems.cs
@@ -0,0 +1,11 @@
+﻿using static Flecs.NET.Bindings.flecs;
+
+namespace NoodleStudios.Flecs;
+
+public unsafe partial struct World
+{
+    // TODO: Add World system API methods here.
+    // https://www.flecs.dev/flecs/group__c__addons__system.html
+
+    // Create, Enable, Disable, Register, Unregister, etc.
+}
