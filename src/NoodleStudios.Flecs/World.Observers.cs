987f659f72199654d603249434c4a9dec84437a8
Joseph Crump <joseph.crump119@gmail.com>
2026-05-21 14:15:28 -0400

    Remove unhelpful world divisions

--- src/NoodleStudios.Flecs/World.Observers.cs
+++
@@ -1,9 +0,0 @@
-﻿using static Flecs.NET.Bindings.flecs;
-
-namespace NoodleStudios.Flecs;
-
-public unsafe partial struct World
-{
-    // TODO: Add World observer API methods here.
-    // https://www.flecs.dev/flecs/group__observers.html
-}
