20cb586fe32755e78e806d41b1d21c9f3ecd0960
Joseph Crump <joseph.crump119@gmail.com>
2026-05-21 14:40:07 -0400

    Delete accidental unnamed .cs file

--- src/NoodleStudios.Flecs/.cs
+++
@@ -1,9 +0,0 @@
-﻿using static Flecs.NET.Bindings.flecs;
-
-namespace NoodleStudios.Flecs;
-
-public unsafe partial struct World
-{
-    // TODO: Add World query API methods here.
-    // https://www.flecs.dev/flecs/group__queries.html
-}
