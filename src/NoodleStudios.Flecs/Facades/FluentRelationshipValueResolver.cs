9323c36c439e5c5012e0ee19cdc73e621a060306
Joseph Crump <joseph.crump119@gmail.com>
2026-05-22 17:02:03 -0400

    Update FluentRelationshipValueResolver.cs

--- src/NoodleStudios.Flecs/Facades/FluentRelationshipValueResolver.cs
+++ src/NoodleStudios.Flecs/Facades/FluentRelationshipValueResolver.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NoodleStudios.Flecs.Facades;
+﻿namespace NoodleStudios.Flecs.Facades;
 
 public unsafe static class FluentRelationshipValueResolver
 {
@@ -17,6 +11,22 @@ public unsafe static class FluentRelationshipValueResolver
         public ref readonly TTarget WithTarget<TTarget>()
             where TTarget : unmanaged
         {
+            var targetId = ComponentId<TTarget>.GetId(_world.Handle);
+            var pairId = _world.Pair(_relation, targetId);
+            return ref _world.Get<TTarget>(_entity, pairId);
+        }
+    }
+
+    public readonly ref struct RelationshipResolver<TSource>(World world, Entity entity, Id relation)
+        where TSource : unmanaged
+    {
+        private readonly World _world = world;
+        private readonly Entity _entity = entity;
+        private readonly Id _relation = relation;
+        public ref readonly TTarget WithTarget<TTarget>()
+            where TTarget : unmanaged
+        {
+            var sourceId = ComponentId<TSource>.GetId(_world.Handle);
             var targetId = ComponentId<TTarget>.GetId(_world.Handle);
             var pairId = _world.Pair(_relation, targetId);
             return ref _world.Get<TTarget>(_entity, pairId);
